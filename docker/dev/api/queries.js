e90120a4870c1bd1b1810f2c82f441a2c1d43606
Lucas Giovani de Paula Salgado <45075913+LucaSalgado@users.noreply.github.com>
2023-06-26 01:54:25 -0300

    refatoração completa

--- docker/dev/api/queries.js
+++ docker/dev/api/queries.js
@@ -141,35 +141,45 @@ function trataMensagem(result, entityType, entityId) {
   return mensagem;
 }
 
+async function verificaContest(contestId, res) {
+  const result = await pool.query(
+    `SELECT contestnumber FROM contesttable WHERE EXISTS (SELECT contestnumber FROM contesttable WHERE contestnumber = ${contestId})`
+  );
+  if (result.rowCount == 0) {
+    throw new Error(
+      "Not Found: O ID da competição especificado na requisição não existe."
+    );
+  }
+}
+
+async function verificaEntityType(body, res) {
+  for (const entity of body.entityTag) {
+    if (!entityTypes.includes(entity.entityType)) {
+      throw new Error(
+        "O ID da competição ou o JSON fornecido no corpo da requisição é inválido."
+      );
+    }
+  }
+}
+
 const postByTag = async (req, res) => {
   const body = req.body;
   try {
-    await pool.query(
-      `SELECT contestnumber FROM contesttable WHERE EXISTS (SELECT contestnumber FROM contesttable WHERE contestnumber = ${req.params.contestId})`
-    );
+    await verificaContest(req.params.contestId, res);
   } catch (error) {
-    res
-      .status(404)
-      .send(
-        "Not Found: O ID da competição especificado na requisição não existe."
-      );
+    res.status(404).send("Bad Request: " + error.message);
+    return;
   }
 
-  for (const entity of body.entityTag) {
-    if (entityTypes.includes(entity.entityType)) {
-    } else {
-      res
-        .status(400)
-        .send(
-          "Bad Request: O ID da competição ou o JSON fornecido no corpo da requisição é inválido."
-        );
-      return;
-    }
+  try {
+    await verificaEntityType(body);
+  } catch (error) {
+    res.status(400).send("Bad Request: " + error.message);
+    return;
   }
 
   try {
     for (const entity of body.entityTag) {
-      let result = undefined;
       if (entity.entityType === "site/user") {
         result = await pool.query(
           `SELECT ${
@@ -196,8 +206,9 @@ const postByTag = async (req, res) => {
           `A entityId = ${entity.entityId} não foi encontrada e portanto suas tags não foram incluídas`
         );
       } else {
+        sucesso = true;
         for (const tag of entity.tag) {
-          const result = await pool.query(
+          await pool.query(
             `INSERT INTO tagstable (entityid, tagid, tagname, tagvalue) VALUES ('${entity.entityId}', ${tag.id}, '${tag.name}', '${tag.value}') ON CONFLICT (entityid, tagid) DO NOTHING;`
           );
         }
@@ -207,49 +218,47 @@ const postByTag = async (req, res) => {
     console.log(error);
   }
 
-  if (entityIdErros.length > 0) {
+  if (entityIdErros.length > 0 && sucesso == true) {
     res.status(207).send({
       message:
         "Multi-Status: Algumas Tags foram incluidas, porem alguns erros ocorreram",
       errors: entityIdErros,
     });
+  } else if (sucesso == false) {
+    res
+      .status(422)
+      .send(
+        "Unprocessable Entity: Todas os entityId enviados não foram encontradas e por tanto elas podem não estar presentes no banco de dados ou as entityId buscadas apresentam erro."
+      );
   } else {
     res.status(204).send("Sucess: tag(s) atualizad(s).");
   }
 
+  sucesso = false;
   entityIdErros = [];
 };
 
 const putByTag = async (req, res) => {
   const body = req.body;
   try {
-    await pool.query(
-      `SELECT contestnumber FROM contesttable WHERE EXISTS (SELECT contestnumber FROM contesttable WHERE contestnumber = ${req.params.contestId})`
-    );
+    await verificaContest(req.params.contestId, res);
   } catch (error) {
-    res
-      .status(404)
-      .send(
-        "Not Found: O ID da competição especificado na requisição não existe."
-      );
+    res.status(404).send("Bad Request: " + error.message);
+    return;
   }
-
-  for (const entity of body.entityTag) {
-    if (entityTypes.includes(entity.entityType)) {
-    } else {
-      res
-        .status(400)
-        .send(
-          "Bad Request: O ID da competição ou o JSON fornecido no corpo da requisição é inválido."
-        );
-      return;
-    }
+  try {
+    await verificaEntityType(body);
+    // Restante do código se todos os entityTypes forem válidos
+  } catch (error) {
+    res.status(400).send("Bad Request: " + error.message);
+    return; // Interrompe a execução do código após enviar a resposta de erro
   }
 
   entityIdErros = [];
   for (const entity of body.entityTag) {
     for (const tag of entity.tag) {
-      const result = await pool.query(
+      result = undefined;
+      result = await pool.query(
         `UPDATE tagstable SET tagname = '${tag.name}', tagvalue = '${tag.value}' WHERE entityid = '${entity.entityId}' AND tagid = ${tag.id}`
       );
       if (result.rowCount === 0) {
@@ -272,39 +281,30 @@ const putByTag = async (req, res) => {
     res
       .status(422)
       .send(
-        "Unprocessable Entity: Todas as tags enviadas não foram encontradas e por tanto elas podem não estar presentes no banco de dados ou as tags buscadas apresentam erro."
+        "Unprocessable Entity: Todas os entityId enviados não foram encontradas e por tanto elas podem não estar presentes no banco de dados ou as entityId buscadas apresentam erro."
       );
   } else {
     res.status(204).send("Sucess: tag(s) atualizad(s).");
   }
 
+  sucesso = false;
   entityIdErros = [];
 };
 
 const deleteByTag = async (req, res) => {
   const body = req.body;
   try {
-    await pool.query(
-      `SELECT contestnumber FROM contesttable WHERE EXISTS (SELECT contestnumber FROM contesttable WHERE contestnumber = ${req.params.contestId})`
-    );
+    await verificaContest(req.params.contestId, res);
   } catch (error) {
-    res
-      .status(404)
-      .send(
-        "Not Found: O ID da competição especificado na requisição não existe."
-      );
+    res.status(404).send("Bad Request: " + error.message);
+    return;
   }
-
-  for (const entity of body.entityTag) {
-    if (entityTypes.includes(entity.entityType)) {
-    } else {
-      res
-        .status(400)
-        .send(
-          "Bad Request: O ID da competição ou o JSON fornecido no corpo da requisição é inválido."
-        );
-      return;
-    }
+  try {
+    await verificaEntityType(body);
+    // Restante do código se todos os entityTypes forem válidos
+  } catch (error) {
+    res.status(400).send("Bad Request: " + error.message);
+    return; // Interrompe a execução do código após enviar a resposta de erro
   }
 
   entityIdErros = [];
@@ -339,6 +339,7 @@ const deleteByTag = async (req, res) => {
     res.status(204).send("Sucess: tag(s) excluída(s).");
   }
 
+  sucesso = false;
   entityIdErros = [];
 };
 
